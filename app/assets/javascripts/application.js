80b2b546f0c31892940939150ea38e8cce0b4eee
victor vitalino <vitalino.victor@gmail.com>
2015-01-26 19:35:22 +0000

    primeiro commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,45 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require js/breakpoints
+//= require js/jquery/jquery-1.11.1.min
+//= require js/masonry.pkgd.min
+//= require js/imagesloaded.pkgd.min
+//= require js/bxslider/jquery.bxslider.min
+//= require js/jquery.carouFredSel-6.2.1-packed
+//= require js/rs-plugin/js/jquery.themepunch.plugins.min 
+//= require js/rs-plugin/js/jquery.themepunch.revolution.min
+//= require js/rs-plugin/videojs/video
+//= require js/jqueryui/jquery-ui
+//= require js/scrollspy
+//= require js/bootstrap-progressbar/bootstrap-progressbar
+//= require js/bootstrap.min
+//= require js/smooth-scroll/SmoothScroll
+//= require js/modules/sliders
+//= require js/modules/ui
+//= require js/modules/retina
+//= require js/modules/animate-numbers
+//= require js/modules/parallax-effect
+//= require js/modules/settings
+//= require js/modules/maps-google
+//= require js/modules/color-themes
+//= require js/radial-progress/jquery.easing.1.3
+//= require js/radial-progress/radialProgress
+//= require js/progressbars
+//= require js/j.placeholder
+//= require js/fancybox/jquery.fancybox.pack
+//= require js/fancybox/jquery.mousewheel.pack
+//= require js/fancybox/jquery.fancybox.pack
+//= require js/user
+//= require js/fontawesome-markers
+//= require js/markerwithlabel
+//= require js/cookie
